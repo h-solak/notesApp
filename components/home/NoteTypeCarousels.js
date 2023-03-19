569bc288a8b3c772cfb31dc81f0befb56d64a2ef
Hasan Solak <81798435+h-solak@users.noreply.github.com>
2023-03-19 16:49:50 +0300

    Tasks on home screen (Pre-view)

--- components/home/NoteTypeCarousels.js
+++ components/home/NoteTypeCarousels.js
@@ -1,11 +1,21 @@
 import React, {useState} from 'react';
-import {ScrollView, View, Text, TouchableOpacity, Image} from 'react-native';
-import AntIcon from 'react-native-vector-icons/AntDesign';
+import {
+  ScrollView,
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  useWindowDimensions,
+} from 'react-native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
 import {useDispatch} from 'react-redux';
 import {selectNoteTypeAndFilter} from '../../redux/slices/noteSlice';
 
 const NoteTypeCarousels = ({navigation}) => {
   const dispatch = useDispatch();
+  const {width, height} = useWindowDimensions();
+
+  const [temp, setTemp] = useState(false);
   const allTypes = [
     {
       name: 'Your\nFavorites',
@@ -16,7 +26,7 @@ const NoteTypeCarousels = ({navigation}) => {
     {
       name: 'Alarmed Notes',
       filter: 'Alarmed Notes',
-      color: '#6D2BCF',
+      color: '#3498db',
       img: 'https://i.ibb.co/4MCPJht/pngwing-com.png',
     },
     {
@@ -32,13 +42,47 @@ const NoteTypeCarousels = ({navigation}) => {
         className="h-40 flex-row gap-3 pl-4 pr-4"
         horizontal={true}
         showsHorizontalScrollIndicator={false}>
+        <TouchableOpacity
+          className={`w-40 rounded-3xl px-5`}
+          style={{width: width * 0.45, backgroundColor: '#8838ff'}}
+          activeOpacity={0.8}
+          onPress={null}>
+          <Text
+            className="text-white font-bold text-xl mt-6 pr-4"
+            style={{lineHeight: 24}}>
+            {'Tasks'}
+          </Text>
+          <View className="my-8" style={{gap: 16}}>
+            {[0, 0, 0].map((item, index) => (
+              <TouchableOpacity
+                key={index}
+                className={`flex-row items-center rounded-full px-2 py-2 ${
+                  temp ? 'bg-white40' : 'bg-white50'
+                }`}
+                style={{gap: 12}}
+                onPress={() => setTemp(!temp)}>
+                <FeatherIcon
+                  name={temp ? 'check-circle' : 'circle'} //check-circle
+                  size={20}
+                  style={{color: temp ? '#ffffff70' : '#ffffff'}}
+                />
+                <Text
+                  className={`${
+                    temp ? 'text-white70 line-through' : 'text-white'
+                  }`}>
+                  draw cash
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        </TouchableOpacity>
         {allTypes.map((item, index) => (
           <TouchableOpacity
             key={index}
-            className={`w-40 rounded-3xl px-5 ${
+            className={`rounded-3xl px-5 ${
               index === allTypes?.length - 1 ? 'mr-8' : null
             }`}
-            style={{backgroundColor: item.color}}
+            style={{width: width * 0.4, backgroundColor: item.color}}
             activeOpacity={0.8}
             onPress={() => {
               dispatch(selectNoteTypeAndFilter(item.filter));
