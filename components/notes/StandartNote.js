a6ceb4ff377428a8f587658a24a7051f2c176a78
Hasan Solak <81798435+h-solak@users.noreply.github.com>
2023-04-15 18:25:43 +0300

    Removed - Slide to delete & archive

--- components/notes/StandartNote.js
+++ components/notes/StandartNote.js
@@ -28,106 +28,67 @@ const StandartNote = ({
 }) => {
   const dispatch = useDispatch();
 
-  const renderLeftActions = (progress, drag, id) => {
-    return (
-      <TouchableOpacity
-        className="rounded-full items-center justify-center"
-        onPress={() => dispatch(archiveNote(id))}>
-        <MaterialIcon name={'archive'} size={24} style={{color: '#ffffff'}} />
-      </TouchableOpacity>
-    );
-  };
-
-  const renderRightActions = (progress, drag, id) => {
-    return (
-      <TouchableOpacity
-        className="rounded-full items-center justify-center"
-        onPress={
-          isTrashScreen
-            ? () => dispatch(permanentlyDeleteNote(id))
-            : () => dispatch(deleteNote(id))
-        }>
-        <FW5Icon name={'trash'} size={17} style={{color: '#ffffff'}} />
-      </TouchableOpacity>
-    );
-  };
-
   return (
     id && (
-      <Swipeable
-        renderLeftActions={(progress, dragX) =>
-          renderLeftActions(progress, dragX, id)
-        }
-        renderRightActions={(progress, dragX) =>
-          renderRightActions(progress, dragX, id)
-        }
-        // onSwipeableOpen={() => closeRow(index)}
-        // ref={ref => (row[index] = ref)}
-        rightOpenValue={-100}>
-        <TouchableOpacity
-          className={`mb-3 px-4 py-2 flex-row rounded-full justify-between items-center`}
-          style={{
-            backgroundColor: selectedNoteIds?.includes(id)
-              ? `${color}80`
-              : color,
-            borderColor: selectedNoteIds?.includes(id)
-              ? '#929292'
-              : color === '#000000'
-              ? '#222222'
-              : color,
-            borderWidth: 2,
-            // opacity: selectedNoteIds?.includes(id) ? 0.6 : 1,
-          }}
-          onPress={() => {
-            if (selectedNoteIds?.length > 0) {
-              handleLongPress(id);
-            } else {
-              dispatch(selectNote(id));
-              navigation.navigate('EditNote');
-            }
-          }}
-          onLongPress={() => handleLongPress(id)}>
-          <View className="flex-row items-center justify-start gap-3">
-            <View className="h-14 w-14 p-2 bg-white items-center justify-center rounded-full">
-              <Text
-                className="text-3xl"
-                style={{color: '#000000', zIndex: 9999}}>
-                {emoji}
-              </Text>
-            </View>
-            <View className="">
-              <Text
-                className={` text-base font-bold ${
-                  title !== '' ? 'text-white' : 'text-slate-300'
-                }`}>
-                {/* BAD! BAAAAAAAAD!!! */}
-                {title !== ''
-                  ? `${title.slice(0, 10)}`
-                  : text.length > 10
-                  ? `${text.slice(0, 10)}...`
-                  : text}
-              </Text>
-              <Text className="text-secondary" style={{fontSize: 12}}>
-                Standard Note
-              </Text>
-            </View>
+      <TouchableOpacity
+        className={`mb-3 px-4 py-2 flex-row rounded-full justify-between items-center`}
+        style={{
+          backgroundColor: selectedNoteIds?.includes(id) ? `${color}80` : color,
+          borderColor: selectedNoteIds?.includes(id)
+            ? '#929292'
+            : color === '#000000'
+            ? '#222222'
+            : color,
+          borderWidth: 2,
+          // opacity: selectedNoteIds?.includes(id) ? 0.6 : 1,
+        }}
+        onPress={() => {
+          if (selectedNoteIds?.length > 0) {
+            handleLongPress(id);
+          } else {
+            dispatch(selectNote(id));
+            navigation.navigate('EditNote');
+          }
+        }}
+        onLongPress={() => handleLongPress(id)}>
+        <View className="flex-row items-center justify-start gap-3">
+          <View className="h-14 w-14 p-2 bg-white items-center justify-center rounded-full">
+            <Text className="text-3xl" style={{color: '#000000', zIndex: 9999}}>
+              {emoji}
+            </Text>
           </View>
-          <TouchableOpacity
-            className="h-10 w-10 rounded-full items-center justify-center p-2"
-            style={
-              {
-                // backgroundColor: 'rgba(255,255,255,0.2)',
-              }
+          <View className="">
+            <Text
+              className={` text-base font-bold ${
+                title !== '' ? 'text-white' : 'text-slate-300'
+              }`}>
+              {/* BAD! BAAAAAAAAD!!! */}
+              {title !== ''
+                ? `${title.slice(0, 10)}`
+                : text.length > 10
+                ? `${text.slice(0, 10)}...`
+                : text}
+            </Text>
+            <Text className="text-secondary" style={{fontSize: 12}}>
+              Standard Note
+            </Text>
+          </View>
+        </View>
+        <TouchableOpacity
+          className="h-10 w-10 rounded-full items-center justify-center p-2"
+          style={
+            {
+              // backgroundColor: 'rgba(255,255,255,0.2)',
             }
-            onPress={() => dispatch(favNote(id))}>
-            <AntIcon
-              name={isFavorite ? 'heart' : 'hearto'}
-              size={22}
-              style={{color: isFavorite ? '#e74c3c' : '#fff'}}
-            />
-          </TouchableOpacity>
+          }
+          onPress={() => dispatch(favNote(id))}>
+          <AntIcon
+            name={isFavorite ? 'heart' : 'hearto'}
+            size={22}
+            style={{color: isFavorite ? '#e74c3c' : '#fff'}}
+          />
         </TouchableOpacity>
-      </Swipeable>
+      </TouchableOpacity>
     )
   );
 };
