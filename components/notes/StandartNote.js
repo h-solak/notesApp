5162f7738530c190b3f78128207ec53429690c2d
Hasan Solak <81798435+h-solak@users.noreply.github.com>
2023-04-16 19:47:27 +0300

    Design changes for standard note

--- components/notes/StandartNote.js
+++ components/notes/StandartNote.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import {View, Text, TouchableOpacity, Button} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Button,
+  useWindowDimensions,
+} from 'react-native';
 import {Swipeable} from 'react-native-gesture-handler';
 import AntIcon from 'react-native-vector-icons/AntDesign';
 import {useDispatch} from 'react-redux';
@@ -27,11 +33,11 @@ const StandartNote = ({
   navigation,
 }) => {
   const dispatch = useDispatch();
-
+  const {height, width} = useWindowDimensions();
   return (
     id && (
       <TouchableOpacity
-        className={`mb-3 px-4 py-2 flex-row rounded-full justify-between items-center`}
+        className={`mb-3 py-2 flex-row rounded-xl justify-between items-center`}
         style={{
           backgroundColor: selectedNoteIds?.includes(id) ? `${color}80` : color,
           borderColor: selectedNoteIds?.includes(id)
@@ -40,6 +46,7 @@ const StandartNote = ({
             ? '#222222'
             : color,
           borderWidth: 2,
+          width: width / 1.1,
           // opacity: selectedNoteIds?.includes(id) ? 0.6 : 1,
         }}
         onPress={() => {
@@ -51,36 +58,39 @@ const StandartNote = ({
           }
         }}
         onLongPress={() => handleLongPress(id)}>
-        <View className="flex-row items-center justify-start gap-3">
-          <View className="h-14 w-14 p-2 bg-white items-center justify-center rounded-full">
-            <Text className="text-3xl" style={{color: '#000000', zIndex: 9999}}>
+        <View
+          className="flex-row items-center justify-start"
+          style={{width: (width / 1.1) * 0.8}}>
+          <View className="p-2 items-center justify-center rounded-full">
+            <Text className="text-2xl" style={{color: '#000000', zIndex: 9999}}>
               {emoji}
             </Text>
           </View>
-          <View className="">
-            <Text
-              className={` text-base font-bold ${
-                title !== '' ? 'text-white' : 'text-slate-300'
-              }`}>
-              {/* BAD! BAAAAAAAAD!!! */}
-              {title !== ''
-                ? `${title.slice(0, 10)}`
-                : text.length > 10
-                ? `${text.slice(0, 10)}...`
-                : text}
-            </Text>
-            <Text className="text-secondary" style={{fontSize: 12}}>
+          <View className="flex-1 flex-col">
+            {title && (
+              <Text
+                style={{fontSize: 14, flexWrap: 'wrap'}}
+                className={`font-bold`}>
+                {title}
+              </Text>
+            )}
+            {text && (
+              <Text style={{fontSize: 13, flexWrap: 'wrap'}} className={``}>
+                {text.slice(0, 200)}
+              </Text>
+            )}
+            {/* <Text className="text-secondary" style={{fontSize: 12}}>
               Standard Note
-            </Text>
+            </Text> */}
           </View>
         </View>
         <TouchableOpacity
-          className="h-10 w-10 rounded-full items-center justify-center p-2"
-          style={
-            {
-              // backgroundColor: 'rgba(255,255,255,0.2)',
-            }
-          }
+          className="flex-1 items-center justify-center"
+          style={{
+            width: (width / 1.1) * 0.2,
+            height: 50,
+            // backgroundColor: 'rgba(255,255,255,0.2)',
+          }}
           onPress={() => dispatch(favNote(id))}>
           <AntIcon
             name={isFavorite ? 'heart' : 'hearto'}
