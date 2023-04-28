b4672e00eb85f9d996e235891e27cde986d3fb23
Hasan Solak <81798435+h-solak@users.noreply.github.com>
2023-04-28 19:05:05 +0300

    Tasks slice initialized (Not working yet)

---
+++ redux/slices/taskSlice.js
@@ -0,0 +1,12 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+export const taskSlice = createSlice({
+  name: 'tasks',
+  initialState: {},
+  reducers: {},
+});
+
+// Action creators are generated for each case reducer function
+export const {} = taskSlice.actions;
+
+export default taskSlice.reducer;
